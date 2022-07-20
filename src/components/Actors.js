3c3eb42eb1ceed13b5b43bc2fbb4c428d044b8b8
Kenneth Albert <gichuka@gmail.com>
2022-07-20 13:44:56 +0300

    Done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { actors } from "../data";
+import { actors, movies } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+
+  return (<div>{/*{code here}*/}
+  <h1>Actors Page</h1>
+  {actors.map((actor,index)=>{
+  return (<div key={index}>
+   <h3>Name:{actor.name}</h3>
+   <p>Movies</p>
+   {actor.movies.map((movie,index)=>{
+   return (<ul key={index}>
+            <li>{movie}</li>
+             </ul>
+     )
+
+   })}
+   
+ </div>)
+  })}
+  
+  
+  
+  </div>);
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return  (<div>
+    <h1>Movies Page</h1>
+    {/*{code here}*/}
+  {movies.map((movie,index)=>{
+  return (
+  
+  <div key={index}>
+   <h3>Name:{movie.title}</h3>
+    <p>Time:{movie.time}</p>
+   {movie.genres.map((genre,index)=>{
+   return (<ul key={index}>
+            <li>{genre}</li>
+             </ul>
+     )
+
+   })}
+   
+ </div>)
+  })}
+  
+  
+  
+  </div>)
 }
 
 export default Movies;
